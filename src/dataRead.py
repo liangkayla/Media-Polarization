95f2d477c3957abcbe44c629caf9b640ae425a9e
Kayla Liang <ksl48@duke.edu>
2024-04-11 14:39:20 -0400

    working on data reading

--- src/dataRead.py
+++ src/dataRead.py
@@ -1,1 +1,16 @@
-name = 'hello'
+import os, json
+import pandas as pd
+
+# finds json files
+path_to_json = 'json/'
+json_files = [pos_json for pos_json in os.listdir(path_to_json) if pos_json.endswith('.json')]
+
+# defining dataframe
+df = pd.DataFrame(columns=['topic', 'source', 'bias', 'url', 'title', 'date', 'authors', 'content', 'content_original', 'source_url', 'bias_text', 'ID'])
+
+# adding json & index number to df
+for index, js in enumerate(json_files):
+    with open(os.path.join(path_to_json, js)) as json_file:
+        json_text = json.load(json_file)
+
+        topic = 
